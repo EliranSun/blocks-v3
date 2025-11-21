9669742f2d3d4af3b5806aa883ca68319d1418f0
Eliran Shemesh <piro27@gmail.com>
2025-11-21 13:17:50 +0200

    Update padding and body styles for layout
    Changed App container from px-4 to p-4 for consistent padding. Removed flex and centering styles from body and adjusted background color handling to improve layout and appearance.

--- src/App.jsx
+++ src/App.jsx
@@ -149,7 +149,7 @@ function App() {
 
   return (
     <section className="w-screen">
-      <div className='px-4 space-y-4'>
+      <div className='p-4 space-y-4'>
         <h1 className='text-left font-bold text-lg merriweather-900'>
           {currentFrame} - {MonthNotes[format(currentDate, 'yyyy-MM')]}
         </h1>
