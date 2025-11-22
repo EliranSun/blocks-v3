700690aeb5109a69f26ae6ba5b900f95ebff5fa2
Eliran Shemesh <piro27@gmail.com>
2025-11-22 16:51:39 +0200

    Update BlocksList.jsx

--- src/BlocksList.jsx
+++ src/BlocksList.jsx
@@ -13,7 +13,7 @@ export const BlocksList = ({ data = [] }) => {
                         "font-bold text-sm grow-0": true,
                         [CategoryColors[item.category.toLowerCase()]]: true,
                     })}>
-                    {item.name}; {format(item.date, "MMM dd, EEEEE"))}
+                    {item.name}; {format(item.date, "MMM dd, E"))}
                 </li>)}
         </ul>
     )
