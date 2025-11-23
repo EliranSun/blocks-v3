4dcd275e86cf07c36661936063fe574bfbf07fd0
Eliran Shemesh <piro27@gmail.com>
2025-11-23 08:23:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -137,7 +137,7 @@ function App() {
           <DailyQuotes />
         </div>
       </div>
-      <div className='m-2 p-4 space-y-4 border rounded-xl'>
+      <div className='m-2 p-4 space-y-4 border shadow rounded-2xl'>
         <CategoryButtons
           selectedCategory={category}
           onCategoryClick={setCategory} />
