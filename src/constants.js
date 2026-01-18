69cf5f165b4842572025996bb7ecd1e80455482b
Eliran Shemesh <piro27@gmail.com>
2026-01-18 18:19:25 +0200

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -41,7 +41,7 @@ export const CategoryBgColors = {
 };
 
 export const MonthNotes = {
-        "2026-01": "Dad to Lokit?!",
+        "2026-01": "Dad to Lokit 🥰",
     "2025-12": "Job Hunt + GameIS Petition",
     "2025-11": "Ramat Gan + Unemployed",
     "2025-10": "Bus Bakerem + Move to Ramat Gan",
